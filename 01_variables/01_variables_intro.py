0d04eb0737e96f57d7bee4e4880dfb4ee31cabc5
Frederick <fjohnson@spartaglobal.com>
2022-01-24 11:38:29 +0000

    stuff

---
+++ 01_variables/01_variables_intro.py
@@ -0,0 +1,9 @@
+a = 1  # int (Integer)
+b = 2  # int
+c = 3.5  # float (floating- point number)
+d = "Hello"  # string
+
+result = (a+c)*b
+print(d)
+
+print(result, type(result))
---
+++ 01_variables/Input.py
@@ -0,0 +1,15 @@
+
+
+
+name = input("What is your name?")
+
+print("Hello," + name)
+
+#  Ask 3 questions
+# Use the stored variables somehow, and print out a response
+# Can you ask questions with numerical answers?
+
+age = int(input("How old are you?"))
+print("You are",age-21, "years older than the answer to 9 + 10")
+Colour = input("What is you favourite colour?")
+print("ewww you like " + Colour)
