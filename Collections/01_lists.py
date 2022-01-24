e8c12e7f830f085214999ebf16be6399fdd38955
Frederick <fjohnson@spartaglobal.com>
2022-01-24 14:23:59 +0000

    Stuff on lists

---
+++ Collections/01_lists.py
@@ -0,0 +1,49 @@
+# lists
+
+# shopping_list = ["eggs", "bread", "bananas", "tea"]
+# print(shopping_list, type(shopping_list))
+#
+# print(len(shopping_list))
+#
+# print(shopping_list[0])
+# print(shopping_list[-1])
+#
+# shopping_list[2] = "milk"
+# print(shopping_list)
+#
+# shopping_list.append("biscuits",)
+# print(shopping_list)
+#
+# shopping_list.remove("bread")
+# print(shopping_list)
+#
+# shopping_list.pop(-1)
+# print(shopping_list)
+
+# mixed = [1, 2, "three", True, None, ["another", "list"]]
+# print(mixed)
+# mixed[1] = 2.0
+# print(mixed)
+#
+# # lists are mutable
+# print(mixed[2:4])
+# print(mixed[4:])
+#
+# print(mixed[2:7:2])
+# print(mixed[2:7:-1])
+# print(mixed[::3])
+#
+# print(mixed[5][0][0])  # list within a list
+#
+# sublist = mixed[5]
+# print(sublist)
+# mixed[5] = ['a', 'b']
+
+colours = ("red", "blue", "green")
+print(colours[0])
+#colours[0] = "maroon"
+
+#  Tuples are immunatable
+
+print(colours.count("red"))
+print(colours.index("green"))
