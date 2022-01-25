55f22150fb43712d21a879d402267019c0d7a383
Frederick <fjohnson@spartaglobal.com>
2022-01-25 18:55:37 +0000

    more fixing but doesn't work

--- 03_control_flow/Fizzbuzz part 2.py
+++ 03_control_flow/Fizzbuzz part 2.py
@@ -1,5 +1,3 @@
-# upper_limity = input("What is your chosen upper limit?\n")
-# lower_limity = input("What is your chosen lower limit?\n")
 def number_input(upper_limit, lower_limit):
     user_input1 = True
     while user_input1:
@@ -12,9 +10,6 @@ def number_input(upper_limit, lower_limit):
 #  The two variables below determine the two words printed by the game when something is
 #  dividable by 3, 5 or 15 without having a remainder
 
-
- #word_at = input("Please enter the word which will be printed when dividable by 3")
-# word_bt = input("Please enter the word which will be printed when dividable by 5")
 
 def word_input(word_a, word_b):
     user_input2 = True
@@ -29,8 +24,6 @@ def word_input(word_a, word_b):
 # The modular determines whether it's a factor of 3,5, or 15 and therefore printing the word when appropriate
 
 def fizzbuzz(upper_limit, lower_limit, word_a, word_b):
-    number_input(upper_limit, lower_limit)
-    word_input(word_a, word_b)
     for i in range(lower_limit, upper_limit):
         if i % 15 == 0:
             print(word_a + word_b)
@@ -40,7 +33,15 @@ def fizzbuzz(upper_limit, lower_limit, word_a, word_b):
             print(word_a)
         else:
             print(i)
+        pass
 
 
-#fizzbuzz(upper_limity=input("What is your chosen upper limit?\n"), lower_limity=input("What is your chosen lower limit?\n"),word_a=input("Please enter the word which will be printed when dividable by 3"), word_b =input("Please enter the word which will be printed when dividable by 5"))
-#fizzbuzz(100,1,"s","fg")
+# word_at = input("Please enter the word which will be printed when dividable by 3")
+# word_bt = input("Please enter the word which will be printed when dividable by 5")
+# upper_limity = input("What is your chosen upper limit?\n")
+# lower_limity = input("What is your chosen lower limit?\n")
+
+# number_input(upper_limity, lower_limity)
+# word_input(upper_limity, lower_limity)
+fizzbuzz(100,1,"fred","rick")
+
